51c4fbc57f3877fca04dd8a42042aa6aaba76d06
ya1ad <aff3ad@gmail.com>
2018-09-14 11:32:22 +0300

    GET /genre/:id

--- index.js
+++ index.js
@@ -24,11 +24,29 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 /**
- * Get all genre
+ * GET all genre
  */
-
 app.get("/api/genres/", (req, res) => {
   res.send(dummy_genre);
 });
 
+/**
+ * GET genre of specific id
+ */
 
+app.get("/api/genre/:id", (req, res) => {
+  const get_genre = find_genre(req.params.id);
+  if (!get_genre) res.status(404).send("Genre not found.");
+  res.send(get_genre);
+});
+
+/**
+ *
+ */
+
+/**
+ * Check genre exist or not
+ */
+function find_genre(id) {
+  return dummy_genre.find(g => g.id === parseInt(id));
+}
