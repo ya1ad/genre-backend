69ba675c764a28484c1635b3fa37750cf58fbffc
ya1ad <aff3ad@gmail.com>
2018-09-14 11:49:10 +0300

    POST /api/genre/

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const Joi = require("joi");
 const app = express();
+
 app.use(express.json());
 
 /**
@@ -41,12 +43,40 @@ app.get("/api/genre/:id", (req, res) => {
 });
 
 /**
- *
+ * POST /api/genres/
  */
+app.post("/api/genres/", (req, res) => {
+  const { error } = nameValidate(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const temp_genre = {
+    id: dummy_genre.length + 1,
+    name: req.body.name
+  };
+  dummy_genre.push(temp_genre);
+  res.send(dummy_genre);
+});
 
 /**
  * Check genre exist or not
  */
 function find_genre(id) {
   return dummy_genre.find(g => g.id === parseInt(id));
+}
+
+/**
+ * Name Validation
+ */
+function nameValidate(genre_name) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(genre_name, schema);
 }
